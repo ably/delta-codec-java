c8dfbfd23c9927cf6114818194f1d6a86701aa48
Tsviatko Yovtchev <tsviatko@codemerx.com>
2019-12-10 16:11:49 +0200

    Adding refactorings and tests

--- delta-codec/src/main/java/io/ably/deltacodec/DeltaApplicationResult.java
+++ delta-codec/src/main/java/io/ably/deltacodec/DeltaApplicationResult.java
@@ -28,13 +28,4 @@ public class DeltaApplicationResult {
     public String asUtf8String() {
         return new String(this.data, StandardCharsets.UTF_8);
     }
-
-    /**
-     * Exports the delta application result as object assuming the bytes in
-     * the result represent an UTF-8 encoded JSON string
-     * @return The object representation of this delta application result
-     */
-    public Object asObject() {
-        return JsonHelper.getInstance().deserialize(this.asUtf8String());
-    }
 }
